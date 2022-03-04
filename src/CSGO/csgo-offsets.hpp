1dd00905162fbbc16162f68d8ff8a676d073e829
ex.codes <kendinikertenkelebek@me.com>
2022-03-04 09:54:49 +0300

    add(game): apex and csgo

---
+++ src/CSGO/csgo-offsets.hpp
@@ -0,0 +1,36 @@
+/*
+MoltenCheat Offsets Repository @MoltenCheat
+Unix Epoch Time: 1646376124
+Visit: https://github.com/MoltenCheat/offsets
+CS:GO Version: 1.38.2.1 (WIP)
+*/
+
+#pragma once
+#include <cstddef>
+
+std::ptrdiff_t client_state = 0x58cfc4;
+std::ptrdiff_t client_state_generic_precache_table = 0x52a4;
+std::ptrdiff_t client_state_signon_state = 0x108;
+std::ptrdiff_t entity_list = 0x4dcde5c;
+std::ptrdiff_t force_attack = 0x31fe370;
+std::ptrdiff_t force_attack2 = 0x31fe37c;
+std::ptrdiff_t force_backward = 0x31fe31c;
+std::ptrdiff_t force_forward = 0x31fe3a0;
+std::ptrdiff_t force_jump = 0x5277d80;
+std::ptrdiff_t force_left = 0x31fe328;
+std::ptrdiff_t force_right = 0x31fe334;
+std::ptrdiff_t gamerules_proxy = 0x52eaddc;
+std::ptrdiff_t global_vars = 0x58ccc8;
+std::ptrdiff_t glow_object_manager = 0x5316258;
+std::ptrdiff_t input = 0x521f430;
+std::ptrdiff_t local_player = 0xdb25dc;
+std::ptrdiff_t player_resource = 0x31fc6c0;
+std::ptrdiff_t radar_base = 0x5202bd4;
+std::ptrdiff_t view_matrix = 0x4dbf774;
+std::ptrdiff_t send_packets = 0xd9542;
+std::ptrdiff_t direct3d_device9 = 0xa5050;
+std::ptrdiff_t studio_hdr = 0x2950;
+std::ptrdiff_t dormant = 0xed;
+std::ptrdiff_t engine_build_number = 0x38d26d4;
+
+// TODO: Added more offsets soon...
