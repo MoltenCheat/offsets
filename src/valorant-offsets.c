52688bfbe08679c70d5f2ec753153a189d367dfe
ex.codes <kendinikertenkelebek@me.com>
2022-03-04 06:07:56 +0300

    add(offset): valorant for c

---
+++ src/valorant-offsets.c
@@ -0,0 +1,56 @@
+/*
+MoltenCheat Offsets Repository @MoltenCheat
+Contributor: xmar from @cheatglobal.com - Thanks for support!
+Unix Epoch Time: 1646360202
+Visit: https://github.com/MoltenCheat/offsets
+League of Legends Version: 12.5 (WIP)
+*/
+
+#define BASEADDRESS
+#define uworld_state 0x86DA078
+#define uworld_key 0x86DA040
+
+/*
+* Classes
+*/
+
+#define game_instance 0x1A8
+#define persistent_level 0x38
+#define component_to_world 0x250
+#define root_component 0x210
+#define root_position 0x164
+#define mesh_component 0x410
+#define actor_array 0xA0
+#define actor_count 0xB8
+#define unique_id 0x38
+#define bone_array 0x558
+#define bone_count 0x560
+#define camera_manager 0x458
+#define camera_position 0x1220
+#define camera_rotation 0x122C
+#define camera_fov 0x1238
+#define local_player_array 0x40
+#define local_player_controller 0x38
+#define local_player_pawn 0x440
+#define control_rotation 0x420
+#define player_state 0x3D0
+#define health 0x1B0
+#define damage_handler 0x928
+#define team_id 0xF8
+#define team_component 0x580
+#define last_render_time 0x350
+#define last_submit_time 0x358
+#define dormant 0x100
+
+// TODO: Need check for 4.04 patch
+#define FresnelIntensity 0x690
+#define FresnelOffset 0x694
+#define bone_component 0x548
+#define relative_location 0x164
+#define relative_rotation 0x170
+#define acknowledged_pawn 0x440
+#define camera_cache_private 0x1f80
+#define player_camera_manager 0x458
+#define player_controller 0x38
+#define cached_life 0x1b0
+#define team 0xf8
